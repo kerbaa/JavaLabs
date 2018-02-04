9f4532654154ad86951a1c788a4aa29ce6c2fbd1
Kerb <akerbashi@gmail.com>
2018-02-04 18:27:41 +0500

    Lab_2_3 (Triangle)

---
+++ Triangle.java
@@ -0,0 +1,14 @@
+public class Triangle
+{
+    public static void main(String args[])
+    {
+        for (int i=1; i<=10;i++)
+        {
+            for (int a=0;a<i;a++)
+            {
+                System.out.print(8);
+            }
+            System.out.println();
+        }
+    }
+}

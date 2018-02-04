95c6bc34ba4d84af1264d63e8505966f01ee6cc3
Kerb <akerbashi@gmail.com>
2018-02-04 18:23:40 +0500

    Lab_2_2 (Rectangle)

---
+++ Rectangle.java
@@ -0,0 +1,16 @@
+public class Rectangle
+{
+    public static void main(String args[])
+    {
+        int m = 2;
+        int n = 4;
+        for (int i= 0;i<m;i++)
+        {
+            for (int j = 0; j<n;j++)
+            {
+                System.out.print(8);
+            }
+            System.out.println();
+        }
+    }
+}

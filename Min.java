4f34c08e91af4f7e1a473259041d4cb3a534d2cd
Kerb <akerbashi@gmail.com>
2018-02-04 18:50:01 +0500

    Lab_2_4(Min_of_Two)

---
+++ Min.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Min
+{
+    public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int m= sc.nextInt();
+        if (n < m)
+        {
+            System.out.print(n);
+        }
+
+
+
+
+
+        else {
+            System.out.print(m);
+        }
+    }
+
+}

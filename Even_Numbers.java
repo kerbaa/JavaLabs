561b811878f64cd029f3d62f2a543029200be756
Kerb <akerbashi@gmail.com>
2018-02-04 18:16:40 +0500

    Lab_2_1 (Even_Numbers)

---
+++ Even_Numbers.java
@@ -0,0 +1,11 @@
+public class Even_Numbers
+{
+    public static void main(String args[])
+    {
+        for (int i=2;i<=100;i=i+2)
+        {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+}
